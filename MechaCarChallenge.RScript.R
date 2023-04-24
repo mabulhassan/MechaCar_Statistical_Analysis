c65e48552d00d5e5788f598b840278bdc8cd8a55
Maha Abul-Hassan <maha3220@yahoo.com>
2023-04-23 22:06:49 -0400

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,7 +1,8 @@
 library(dplyr)
+
 library(tidyverse)
 
-df=read_csv('MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
+df=read.csv('MechaCar_mpg.csv', check.names=F,stringsAsFactors = F)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=df)
 
@@ -11,3 +12,25 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 lm(mpg ~ vehicle_length + ground_clearance,data=df)
 summary(lm(mpg ~ vehicle_length + ground_clearance,data=df))
 
+sus_coil_df=read.csv('Suspension_Coil.csv', check.names=F,stringsAsFactors = F)
+
+total_summary <- sus_coil_df %>% summarize(sus_coli_mean=mean(PSI),
+                                           sus_coli_median=median(PSI),
+                                           sus_coli_variance=var(PSI),
+                                           sus_coli_std_dev=sd(PSI),Num_Coil=n(),
+                                           .groups = 'keep') 
+
+lot_summary <- sus_coil_df  %>% group_by(Manufacturing_Lot) %>% summarize(sus_coli_mean=mean(PSI),
+                                                                         sus_coli_median=median(PSI),
+                                                                         sus_coli_variance=var(PSI),
+                                                                         sus_coli_std_dev=sd(PSI),Num_Coil=n(),
+                                                                         .groups = 'keep') 
+t.test(sus_coil_df$PSI,mu=1500)
+
+lot1 <- subset(sus_coil_df, Manufacturing_Lot=="Lot1")
+lot2 <- subset(sus_coil_df, Manufacturing_Lot=="Lot2")
+lot3 <- subset(sus_coil_df, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
